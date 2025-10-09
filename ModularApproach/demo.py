933f39c432b78088aaac831cba2dcf5f0b681f6c
yadavsangam1984 <yadavsangam1984@gmail.com>
2025-10-09 16:52:07 +0530

    done

---
+++ ModularApproach/demo.py
@@ -0,0 +1,10 @@
+def sum(num1,num2):     #function definition
+    num3=num1+num2      #expression 
+    return num3           #return the value num3
+
+
+#testing
+'''
+ans=sum(100,200)
+print(ans)
+'''
